8cf1114512766c7d5a2514e78a8b6d1464f35bea
kunal.mishra <mishrakunal012@gmail.com>
2025-11-30 19:42:53 +0530

    added the first commit

---
+++ src/main/java/com/kunal/analytics/logmonitoring/LogMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.analytics.logmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/analytics/logmonitoring/LogMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.analytics.logmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
